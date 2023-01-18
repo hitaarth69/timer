95bb1e8b27fda0d51bb68f8f9268d32b853d575f
hitaarth69 <104824579+hitaarth69@users.noreply.github.com>
2023-01-18 19:43:49 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,12 @@
+def calculator(num_1,num_2):
+    
+
+   
+   print(num_1 + num_2)
+   print(num_1 - num_2)
+   print(num_1 * num_2)
+   print(num_1 / num_2)
+   print(num_1 % num_2)
+num_1= int(input("Enter the first number: "))
+num_2=int(input("Enter the Second number: "))
+calculator(num_1,num_2)
